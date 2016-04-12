b4bb5fc62aa4f63bfc6c43b80b64a857b9259ca2
Alec Yach <ayach@auvik.com>
2016-04-11 21:29:05 -0400

    END WORK

--- client/main.js
+++ client/main.js
@@ -7,6 +7,7 @@ import '../imports/ui/blog.js';
 Template.body.onCreated(function() {
   Session.set("currentUser", null);
 });
+
 Template.body.helpers({
   validUser: function() {
     return Session.get("currentUser") !== null;
